94b4c7339b08617b18ebfd12d8258d435eb3ff2b
Bommineni Srinivasulu <srinivasulu.bommineni24@gmail.com>
2025-11-26 23:57:27 +0530

    Update User.ts

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,5 +1,10 @@
-// src/entities/User.ts
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  type Relation,
+} from 'typeorm';
 import { FcmToken } from './FcmToken.js';
 import { VideoProgress } from './VideoProgress.js';
 import { Notification } from './Notification.js';
@@ -16,14 +21,14 @@ export class User {
   email!: string;
 
   @Column({ type: 'varchar', length: 200 })
-  password!: string; // TODO: hash in production
+  password!: string; // store hashed password in production
 
-  @OneToMany(() => FcmToken, (t) => t.user)
-  fcmTokens!: FcmToken[];
+  @OneToMany(() => FcmToken, (token) => token.user)
+  fcmTokens!: Relation<FcmToken[]>;
 
-  @OneToMany(() => VideoProgress, (p) => p.user)
-  progress!: VideoProgress[];
+  @OneToMany(() => VideoProgress, (progress) => progress.user)
+  progress!: Relation<VideoProgress[]>;
 
-  @OneToMany(() => Notification, (n) => n.user)
-  notifications!: Notification[];
+  @OneToMany(() => Notification, (notification) => notification.user)
+  notifications!: Relation<Notification[]>;
 }
