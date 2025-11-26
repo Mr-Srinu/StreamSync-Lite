f2717b6520f57cb28a6ebbdaa88b39935d7d8514
Bommineni Srinivasulu <srinivasulu.bommineni24@gmail.com>
2025-11-26 23:56:47 +0530

    Update FcmToken.ts

--- backend/src/entities/FcmToken.ts
+++ backend/src/entities/FcmToken.ts
@@ -1,10 +1,10 @@
-// src/entities/FcmToken.ts
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   ManyToOne,
   Unique,
+  type Relation,
 } from 'typeorm';
 import { User } from './User.js';
 
@@ -14,15 +14,20 @@ export class FcmToken {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-  @ManyToOne(() => User, (u) => u.fcmTokens, { eager: false })
-  user!: User;
+  @ManyToOne(() => User, (user) => user.fcmTokens, {
+    onDelete: 'CASCADE',
+  })
+  user!: Relation<User>;
 
   @Column({ type: 'varchar', length: 500 })
   token!: string;
 
   @Column({ type: 'varchar', length: 20 })
-  platform!: string;
+  platform!: string; // e.g. "android", "ios", "web"
 
-  @Column({ type: 'datetime' })
+  @Column({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
   createdAt!: Date;
 }
